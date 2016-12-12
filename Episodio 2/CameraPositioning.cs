ce098fd3201e98185233303915995c0d83a0a958
Baenor <baenor@live.it>
2016-12-12 21:59:05 +0100

    Episodio 2

---
+++ Episodio 2/CameraPositioning.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositioning : MonoBehaviour {
+
+    public float z_offset;
+
+	// Use this for initialization
+	void Start () {
+        float x = WallGeneration.mapGeneration.cols / 2f;
+        float y = WallGeneration.mapGeneration.rows / 2f;
+
+        transform.position = new Vector3(x, y, z_offset);
+	}
+	
+}
---
+++ Episodio 2/SnakeMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour {
+    
+    Vector2 dir;
+
+    public float movementDelay = 0.2f;
+    float timePassed = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckMovement();
+	}
+
+    void CheckMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            dir = Vector2.down;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            dir = Vector2.up;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            dir = Vector2.left;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            dir = Vector2.right;
+        }
+
+        if(timePassed > movementDelay)
+        {
+            timePassed = 0f;
+            //applicato il movimento
+            ApplyMovement();
+        }
+        else
+        {
+            timePassed += Time.deltaTime;
+        }
+    }
+
+    void ApplyMovement()
+    {
+        transform.Translate(dir);
+    }
+}
